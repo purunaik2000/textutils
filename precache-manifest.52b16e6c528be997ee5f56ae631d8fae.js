ce694a80dfbb97ce3c7479e0be6d7f4bfa0ccbb7
purunaik2000 <98662863+purunaik2000@users.noreply.github.com>
2022-09-26 22:47:59 +0530

    Updates

---
+++ precache-manifest.52b16e6c528be997ee5f56ae631d8fae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ee2969cec37b4905d5c",
+    "url": "/textutils/static/css/main.7ca895ac.chunk.css"
+  },
+  {
+    "revision": "4ee2969cec37b4905d5c",
+    "url": "/textutils/static/js/main.4ee2969c.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/textutils/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "3e1bda06019c7aa43dc6",
+    "url": "/textutils/static/js/2.3e1bda06.chunk.js"
+  },
+  {
+    "revision": "630f10c0371371d002cf",
+    "url": "/textutils/static/js/runtime~main.630f10c0.js"
+  },
+  {
+    "revision": "130f59919e50ac5df70059f994139521",
+    "url": "/textutils/index.html"
+  }
+];
